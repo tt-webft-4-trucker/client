6da7b7d7e118b3dd1fc5b6b77a0093dfc5634871
mwbrennan <mwb5@me.com>
2021-02-01 20:40:06 -0600

    view truck & menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import FoodTruckView from './FoodTruckView';
+
+
 
 function App() {
   return (
     <div className="App">
         <h1>Some App</h1>
+        <Switch>
+          <Route path='/trucks/:id'>
+            <FoodTruckView />
+          </Route>
+        </Switch>
     </div>
   );
 }
---
+++ src/FoodTruckView.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+import axios from 'axios';
+
+function FoodTruckView(props) {
+    const [ truck, setTruck ] = useState();
+    
+    const { id } = useParams();
+
+      useEffect(() => {
+        // console.log("id is : ")
+        console.log(id);
+          axios
+            .get(`https://truck-server.herokuapp.com/trucks/${id}`)
+            .then((res) => {
+                // console.log(res.data)
+                setTruck(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+    }, [id]);
+
+
+  if (!truck) {
+    return <div>Loading truck information...</div>;
+  }
+
+    return (
+    <div className="FoodTruckView">
+        <TruckName>{truck.name}</TruckName>
+
+            <MenuContainer>
+
+                {
+                    truck.menu.map(item => {
+                        return (
+                                <MenuItemStyle>
+                                    <h3 className='itemName'>{item.item_name}</h3>
+                                    <p className='itemDescription info'>{item.item_description}</p>
+                                    <p className='itemPrice info'>Price: {item.item_price}</p>
+                                    {/* <p className='customerRatings'>{item.customerRatings}</p> */}
+                                    <p className='avgCustomerRatings info'>Average Rating: {truck.customer_rating_avg}</p>
+                                    <div className='foodImage'>
+                                        <img src={item.item_photos} />
+                                    </div>
+                                </MenuItemStyle>
+                        )
+                    })
+                }
+        
+            </MenuContainer>
+
+    </div>
+  );
+}
+
+export default FoodTruckView;
+
+
+
+const TruckName = styled.h1`
+    display: flex;
+    margin-left: 3rem;
+    color: teal;
+`
+
+const MenuContainer = styled.div`
+    background-color: #F2F2F2;
+    opacity: 80%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+
+`
+
+const MenuItemStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #F9F9F9;
+    border: 1px solid #3C3C3C;
+    width: 26rem;
+    margin: 2rem;
+    border-radius: .5rem;
+    padding: 1rem;
+    color: #3C3C3C;
+
+    .itemName {
+        display: flex;
+        color: #3C3C3C;
+        margin-top: 2px;
+    }
+
+    .itemDescription {
+        display: flex;
+        text-align: left;
+        margin-top: -4px;
+    }
+
+    .info {
+        display: flex;
+        margin-top: 6px;
+    }
+
+
+    .foodImage {
+        display: flex;
+        justify-content: flex-start;
+        margin-top: 6px;
+    }
+
+    img {
+        display: flex; 
+        width: 10rem;
+        align-items: center;
+        border-radius: 8px;
+    }
+`
+
